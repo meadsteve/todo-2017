a13ceb6e5bbb86c980e721f9d019e413ead41d32
steve.brazier <steve.brazier@just-eat.com>
2017-09-20 19:55:49 +0100

    group the tests

--- src/todolist.test.js
+++ src/todolist.test.js
@@ -1,31 +1,36 @@
 import {actions} from "./todolist"
 
-test('Action: Set new title applies to the next todo', () => {
-    const startingState = {
-        todos: [],
-        nextTodo: {text: "", id: 0}
-    };
-    const updatedState = actions.setNewTitle(startingState, {}, "do the thing");
+describe('Action: setNewTitle', () => {
+    test('Updates the title of the next todo', () => {
+        const startingState = {
+            todos: [],
+            nextTodo: {text: "", id: 0}
+        };
+        const updatedState = actions.setNewTitle(startingState, {},
+            "do the thing");
 
-    expect(updatedState.nextTodo).toEqual({text: "do the thing", id: 0});
+        expect(updatedState.nextTodo).toEqual({text: "do the thing", id: 0});
+    });
 });
 
-test('Action: addTodo moves the current nextTodo on to the list', () => {
-    const startingState = {
-        todos: [],
-        nextTodo: {text: "first thing", id: 0}
-    };
-    const updatedState = actions.addTodo(startingState);
+describe('Action: addTodo', () => {
+    test('Moves the current nextTodo on to the list', () => {
+        const startingState = {
+            todos: [],
+            nextTodo: {text: "first thing", id: 0}
+        };
+        const updatedState = actions.addTodo(startingState);
 
-    expect(updatedState.todos).toEqual([{text: "first thing", id: 0}]);
-});
+        expect(updatedState.todos).toEqual([{text: "first thing", id: 0}]);
+    });
 
-test('Action: addTodo moves resets the next todo and increments the id', () => {
-    const startingState = {
-        todos: [],
-        nextTodo: {text: "fourth thing", id: 4}
-    };
-    const updatedState = actions.addTodo(startingState);
+    test('Resets the next todo and increments the id', () => {
+        const startingState = {
+            todos: [],
+            nextTodo: {text: "fourth thing", id: 4}
+        };
+        const updatedState = actions.addTodo(startingState);
 
-    expect(updatedState.nextTodo).toEqual({text: "", id:5});
+        expect(updatedState.nextTodo).toEqual({text: "", id: 5});
+    });
 });
